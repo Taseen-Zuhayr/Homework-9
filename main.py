a332e88c1697dc6fb5392c7e58c43140c2838709
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-10-04 14:52:27 +0600

    Homework 9

---
+++ main.py
@@ -0,0 +1,11 @@
+x = int(input("Enter your birth year : "))
+age = 2024-x
+
+if age<=20 and age>=10:
+    print("You can join my class.")
+elif age<10 and age>0:
+    print("You are too young to join my class.")
+elif age>20:
+    print("You are too old to join my class.")
+else:
+    print("Are you a time traveller?")
